fea7bfa57160f4b422fc27c7af82797e364dde5e
novapc74 <novapc74@yandex.ru>
2025-04-08 11:50:42 +0300

    add measure & property tables

--- project/src/Enum/Database/CreateTable.php
+++ project/src/Enum/Database/CreateTable.php
@@ -2,6 +2,12 @@
 
 namespace App\Enum\Database;
 
+/**
+ * Важен порядок создания таблиц.
+ * Первая таблица сверху, последняя снизу.
+ * По хорошему - можно сделать все через миграции eloquent, но не хочется "утяжелять" фреймворк.
+ * Возможно, реализую создание/удаление таблиц через консольную команду.
+ */
 enum CreateTable: string
 {
     case CREATE_CITY = "CREATE TABLE cities (
@@ -9,6 +15,16 @@ enum CreateTable: string
     title VARCHAR(50) NOT NULL,
     slug VARCHAR(50) NOT NULL UNIQUE
 )";
+    case CREATE_MEASURE = "CREATE TABLE measures (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL UNIQUE
+                      )";
+    case CREATE_PROPERTY = "CREATE TABLE properties (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL UNIQUE,
+    is_invisible TINYINT(1) NOT NULL DEFAULT 0,
+    measure_id SMALLINT,
+    FOREIGN KEY (measure_id) REFERENCES measures(id))";
     case CREATE_CATEGORY = "CREATE TABLE categories (
     id UUID PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
