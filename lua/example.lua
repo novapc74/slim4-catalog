69e1da828012c082339627b45128c8f311ed320e
novapc74 <novapc74@yandex.ru>
2025-04-10 16:27:30 +0300

    test lua json

--- lua/example.lua
+++ lua/example.lua
@@ -1,2 +1,17 @@
--- ngx.header.content_type = "text/plain; charset=utf-8"
-ngx.say("Мы начинаем писать на LUA?!")
+local cjson = require "cjson"  -- Подключаем библиотеку cjson
+
+local my_var = ngx.var.my_var  -- Получаем значение переменной Nginx
+
+-- Создаем таблицу для JSON
+local json_table = {
+    message = my_var,
+    data = {
+        message = "try again :)"
+        }
+}
+
+-- Сериализуем таблицу в JSON
+local json_response = cjson.encode(json_table)
+
+-- Отправляем JSON-ответ
+ngx.say(json_response)
